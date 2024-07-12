b2b5547f9eea3ae1ebe1e3a45199f8c799c7e7c3
Hossam <7osssam@protonmail.ch>
2024-07-12 18:33:52 +0300

    Add handleUpdateEmail and handleUpdatePassword unit tests

---
+++ tests/handleUpdateEmail/handleUpdateEmail_test.cpp
@@ -0,0 +1,124 @@
+
+#include "Setup.h"
+
+using namespace testing;
+
+TEST_F(handleUpdateEmailTest, onSuccessfulUpdateEmail)
+{
+	QByteArray requestData = R"(
+		{
+			"Request": 12,
+			"Data": {
+				"email": "user1@example.com",
+				"password": "pass1",
+				"new_email": "New@mail.com"
+			}
+		}
+	)";
+
+	QByteArray expectedResponse = R"(
+		{
+			"Response": 12,
+			"Data": {
+				"status": 1,
+				"message": "Email updated successfully"
+			}
+		}
+	)";
+
+	// get user id of old email
+	DB::DbResult result = dbManager_->select("id")->table("users")->where("email =", "user1@example.com")->exec();
+
+	EXPECT_THAT(result.size(), Eq(1));
+
+	int userId_1 = result.data(0).value("id").toInt();
+
+	QByteArray actualResponse = handler_->makeRequest(requestData);
+
+	// Ensure to strip whitespace before comparing
+	QJsonDocument expectedDoc = QJsonDocument::fromJson(expectedResponse.trimmed());
+	QJsonDocument actualDoc = QJsonDocument::fromJson(actualResponse.trimmed());
+
+	qDebug() << "Expected: " << expectedDoc;
+	qDebug() << "Actual: " << actualDoc;
+
+	EXPECT_EQ(expectedDoc, actualDoc);
+
+	//
+	result = dbManager_->select("id")->table("users")->where("email =", "New@mail.com")->exec();
+	EXPECT_THAT(result.size(), Eq(1));
+
+	int userId_2 = result.data(0).value("id").toInt();
+
+	EXPECT_EQ(userId_1, userId_2); // Ensure the user id remains the same
+}
+
+TEST_F(handleUpdateEmailTest, onEmailAlreadyExists)
+{
+	QByteArray requestData = R"(
+		{
+			"Request": 12,
+			"Data": {
+				"email": "user1@example.com",
+				"password": "pass1",
+				"new_email": "user2@example.com"
+			}
+		}
+	)";
+
+	QByteArray expectedResponse = R"(
+		{
+			"Response": 12,
+			"Data": {
+				"status": 0,
+				"message": "Email already exists"
+			}
+		}
+	)";
+
+	QByteArray actualResponse = handler_->makeRequest(requestData);
+
+	// Ensure to strip whitespace before comparing
+	QJsonDocument expectedDoc = QJsonDocument::fromJson(expectedResponse.trimmed());
+	QJsonDocument actualDoc = QJsonDocument::fromJson(actualResponse.trimmed());
+
+	qDebug() << "Expected: " << expectedDoc;
+	qDebug() << "Actual: " << actualDoc;
+
+	EXPECT_EQ(expectedDoc, actualDoc);
+}
+
+TEST_F(handleUpdateEmailTest, onInvalidPassword)
+{
+	QByteArray requestData = R"(
+		{
+			"Request": 12,
+			"Data": {
+				"email": "user1@example.com",
+				"password": "pass2",
+				"new_email": "New@mail.com"
+			}
+		}
+	)";
+
+	QByteArray expectedResponse = R"(
+		{
+			"Response": 12,
+			"Data": {
+				"status": 0,
+				"message": "Invalid password"
+			}
+		}
+	)";
+
+	QByteArray actualResponse = handler_->makeRequest(requestData);
+
+	// Ensure to strip whitespace before comparing
+	QJsonDocument expectedDoc = QJsonDocument::fromJson(expectedResponse.trimmed());
+	QJsonDocument actualDoc = QJsonDocument::fromJson(actualResponse.trimmed());
+
+	qDebug() << "Expected: " << expectedDoc;
+	qDebug() << "Actual: " << actualDoc;
+
+	EXPECT_EQ(expectedDoc, actualDoc);
+}
---
+++ tests/handleUpdatePassword/handleUpdatePassword_test.cpp
@@ -0,0 +1,89 @@
+
+#include "Setup.h"
+
+using namespace testing;
+
+TEST_F(handleUpdatePasswordTest, onSuccessfulUpdatePassword)
+{
+	QByteArray requestData = R"(
+		{
+			"Request": 13,
+			"Data": {
+				"email": "user1@example.com",
+				"password": "pass1",
+				"new_password": "NewPassword"
+			}
+		}
+	)";
+
+	QByteArray expectedResponse = R"(
+		{
+			"Response": 13,
+			"Data": {
+				"status": 1,
+				"message": "Password updated successfully"
+			}
+		}
+	)";
+
+	// get user id of old email
+	DB::DbResult result = dbManager_->select("id")->table("users")->where("password =", "pass1")->exec();
+
+	EXPECT_THAT(result.size(), Eq(1));
+
+	int userId_1 = result.data(0).value("id").toInt();
+
+	QByteArray actualResponse = handler_->makeRequest(requestData);
+
+	// Ensure to strip whitespace before comparing
+	QJsonDocument expectedDoc = QJsonDocument::fromJson(expectedResponse.trimmed());
+	QJsonDocument actualDoc = QJsonDocument::fromJson(actualResponse.trimmed());
+
+	qDebug() << "Expected: " << expectedDoc;
+	qDebug() << "Actual: " << actualDoc;
+
+	EXPECT_EQ(expectedDoc, actualDoc);
+
+	//
+	result = dbManager_->select("id")->table("users")->where("password =", "NewPassword")->exec();
+	EXPECT_THAT(result.size(), Eq(1));
+
+	int userId_2 = result.data(0).value("id").toInt();
+
+	EXPECT_EQ(userId_1, userId_2); // Ensure the user id remains the same
+}
+
+TEST_F(handleUpdatePasswordTest, onInvalidPassword)
+{
+	QByteArray requestData = R"(
+		{
+			"Request": 13,
+			"Data": {
+				"email": "user1@example.com",
+				"password": "notuser1password",
+				"new_password": "NewPassword"
+			}
+		}
+	)";
+
+	QByteArray expectedResponse = R"(
+		{
+			"Response": 13,
+			"Data": {
+				"status": 0,
+				"message": "Invalid password"
+			}
+		}
+	)";
+
+	QByteArray actualResponse = handler_->makeRequest(requestData);
+
+	// Ensure to strip whitespace before comparing
+	QJsonDocument expectedDoc = QJsonDocument::fromJson(expectedResponse.trimmed());
+	QJsonDocument actualDoc = QJsonDocument::fromJson(actualResponse.trimmed());
+
+	qDebug() << "Expected: " << expectedDoc;
+	qDebug() << "Actual: " << actualDoc;
+
+	EXPECT_EQ(expectedDoc, actualDoc);
+}
