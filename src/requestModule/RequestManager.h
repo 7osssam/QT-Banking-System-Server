1500a121345f745c5f914ab5ef7deb942fb93f03
Hossam <7osssam@protonmail.ch>
2024-07-20 23:34:08 +0300

    Refactor RequestManager class

--- src/requestModule/RequestManager.h
+++ src/requestModule/RequestManager.h
@@ -39,7 +39,27 @@ private:
      * @param parseError A QJsonParseError object that contains details about the parsing error.
      * @return A QJsonObject containing the error response with an error message and status.
      */
-	QJsonObject handleJsonParseError(const QJsonParseError& parseError);
+	QJsonObject handleJsonParseError(const QJsonParseError& parseError)
+	{
+		QJsonObject response;
+		QJsonObject data;
+
+		response.insert("Response", -1);
+
+		data.insert("status", int(false));
+		data.insert("message", "JSON Parse Error: " + parseError.errorString());
+
+		response.insert("Data", data);
+
+		// Convert response to JSON
+		QJsonDocument responseDoc(response);
+		QByteArray	  responseData = responseDoc.toJson();
+
+		// Send response
+		qDebug().noquote() << "<-- JSON Parse Error::Response :\n" << responseDoc.toJson(QJsonDocument::Indented);
+
+		return response;
+	}
 
 public:
 	/**
@@ -70,7 +90,21 @@ public:
      *
      * @param Mutex A pointer to a QMutex used to ensure thread-safe operations.
      */
-	RequestManager(QMutex* Mutex);
+	RequestManager(QMutex* Mutex) : Mutex_(Mutex)
+	{
+		requests_[Login] = new LoginRequest();
+		requests_[GetAccountnumber] = new GetAccountNumberRequest();
+		requests_[GetBalance] = new GetBalanceRequest();
+		requests_[GetTransactionsHistory] = new GetTransactionsHistoryRequest();
+		requests_[GetDatabase] = new GetDatabaseRequest();
+		requests_[MakeTransaction] = new MakeTransactionRequest();
+		requests_[CreateNewUser] = new CreateNewUserRequest();
+		requests_[DeleteUser] = new DeleteUserRequest();
+		requests_[UpdateUser] = new UpdateUserRequest();
+		requests_[UserInit] = new UserInitRequest();
+		requests_[UpdateEmail] = new UpdateEmailRequest();
+		requests_[UpdatePassword] = new UpdatePasswordRequest();
+	}
 
 	/**
      * @brief Processes an incoming request and generates an appropriate response.
@@ -81,14 +115,61 @@ public:
      * This method parses the incoming JSON data, determines the type of request, and delegates the processing
      * to the corresponding request handler. It also handles JSON parsing errors and generates error responses if needed.
      */
-	QByteArray makeRequest(QByteArray data);
+	QByteArray makeRequest(QByteArray data)
+	{
+		int				requestNumber;
+		QJsonParseError parseError;
+		QJsonDocument	jsonDoc = QJsonDocument::fromJson(data, &parseError);
+
+		qDebug().noquote() << "--> Request received: " << jsonDoc.toJson(QJsonDocument::Indented);
+
+		QJsonObject jsonObjResponse;
+
+		QJsonObject jsonObjRequest = jsonDoc.object();
+
+		do
+		{
+			if (parseError.error == QJsonParseError::NoError)
+			{
+				requestNumber = jsonObjRequest.value("Request").toInt();
+			}
+			else
+			{
+				qCritical() << "JSON Parse Error: " << parseError.errorString();
+				jsonObjResponse = handleJsonParseError(parseError);
+				break;
+			}
+
+			if (requests_.contains(requestNumber))
+			{
+				//Request* request = requests_[requestNumber];
+				jsonObjResponse = requests_[requestNumber]->execute(jsonObjRequest, *Mutex_);
+			}
+			else
+			{
+				qCritical() << "Request not found";
+				qCritical() << "Invalid request: " << requestNumber;
+			}
+		} while (false);
+
+		QJsonDocument responseDoc(jsonObjResponse);
+		QByteArray	  responseByteArr = responseDoc.toJson();
+
+		return responseByteArr;
+	}
 
 	/**
      * @brief Destructor for the RequestManager.
      *
      * Cleans up any resources allocated by the RequestManager.
      */
-	~RequestManager();
+	~RequestManager()
+	{
+		//for (auto request: requests_)
+		//{
+		//	delete request;
+		//}
+	}
 };
 
 #endif // REQUESTMANAGER_H
